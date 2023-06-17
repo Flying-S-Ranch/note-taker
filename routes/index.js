c7d79fbf893942028fcab80bc9aac1201587e66a
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-16 21:29:35 -0500

    edits the routes folder

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const notesRouter = require('./notes')
 
+// import express from 'express'
 const app = express();
 
 app.use('/notes', notesRouter);
--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,14 @@
 // This file is organized following the example of the mini-project
-const notes = require('express').Router();
+const notes = require('express');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils')
 const { v4: uuidv4 } = require('uuid');
+
+// const app = express();
+// import express from 'express'
+const app = express();
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 module.exports = notes;
