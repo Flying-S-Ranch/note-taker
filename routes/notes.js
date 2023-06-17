20b2930541e9ca6d10f95f3c9475c362e1b97739
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-16 22:21:04 -0500

    adds post command to add new notes

--- routes/notes.js
+++ routes/notes.js
@@ -4,9 +4,25 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtils')
 const { v4: uuidv4 } = require('uuid');
 
 // GET: /api/notes
-
 notes.get('/', (req, res) => {
     readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+})
+
+// POST: /api/notes
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        readAndAppend(newNote, './db/notes.json');
+        res.json(`New note added`);
+    } else {
+        res.errored('Not able to add note. Check required fields')
+    }
 })
 
 module.exports = notes;
